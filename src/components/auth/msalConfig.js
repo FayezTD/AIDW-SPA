41e79964c232468bcb5c7d3c33c845d0848ddf0b
Fayez-2403 <mfk.fayez@gmail.com>
2025-02-27 23:07:41 +0530

    Clear cached files

--- src/components/auth/msalConfig.js
+++ src/components/auth/msalConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: process.env.REACT_APP_AZURE_CLIENT_ID, 
     authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_TENANT_ID}`, 
-    redirectUri: process.env.REACT_APP_REDIRECT_URI || 'https://salmon-plant-0706ca50f.4.azurestaticapps.net/',
+    redirectUri: process.env.REACT_APP_REDIRECT_URI || 'https://salmon-plant-0706ca50f.4.azurestaticapps.net',
   },
   cache: {
     cacheLocation: 'sessionStorage',
