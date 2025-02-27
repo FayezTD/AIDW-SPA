6708c6e400035d4e61928c7ad59d75b8dd995c07
Fayez-2403 <mfk.fayez@gmail.com>
2025-02-27 23:00:39 +0530

    fixed redirect uri

--- src/components/auth/msalConfig.js
+++ src/components/auth/msalConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: process.env.REACT_APP_AZURE_CLIENT_ID, 
     authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_TENANT_ID}`, 
-    redirectUri: process.env.REACT_APP_REDIRECT_URI || 'http://localhost:3000',
+    redirectUri: process.env.REACT_APP_REDIRECT_URI || 'https://salmon-plant-0706ca50f.4.azurestaticapps.net/',
   },
   cache: {
     cacheLocation: 'sessionStorage',
