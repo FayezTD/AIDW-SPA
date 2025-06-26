d071c7fec4cf295cccd4c6b0a3cbc1d75bc03912
FayezTD <fayez@thirdeyedata.ai>
2025-06-26 11:47:53 +0530

    stg env TED Webapp

--- src/components/auth/msalConfig.js
+++ src/components/auth/msalConfig.js
@@ -46,8 +46,8 @@ export const msalConfig = {
   auth: {
     clientId: process.env.REACT_APP_AZURE_CLIENT_ID, 
     authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_TENANT_ID}`, 
-    redirectUri: process.env.REACT_APP_REDIRECT_URI || 'https://aidw-web-application-daaggeafcbfteuhc.westus2-01.azurewebsites.net/',
-    postLogoutRedirectUri: process.env.REACT_APP_REDIRECT_URI || 'https://aidw-web-application-daaggeafcbfteuhc.westus2-01.azurewebsites.net/',
+    redirectUri: process.env.REACT_APP_REDIRECT_URI || 'https://salmon-plant-0706ca50f.4.azurestaticapps.net/',
+    postLogoutRedirectUri: process.env.REACT_APP_REDIRECT_URI || 'https://salmon-plant-0706ca50f.4.azurestaticapps.net/',
     navigateToLoginRequestUrl: false, // Changed from true to false
   },
   cache: {
